a2ec5587b44853e9109719ce08504148229af774
Kendrick Ledet <kendrickledet@gmail.com>
2013-06-20 18:47:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+gplus_event - CLI Google+ Event Manager
+==========================================
+You can use this script to automate the creation and editing of Google+ Events, and to read some Event details.
+
+Dependencies
+------------
+    * Python 2.6+
+    * splinter module  (for browser automation)
+
+Settings
+--------
+    * Fill out your username and password in config.json
+
+
+Usage
+-----
+    * TODO
---
+++ gplus_event.py
@@ -0,0 +1,111 @@
+from splinter import Browser
+from time import sleep
+import argparse, json
+
+class GPlusEventManager(object):
+    def __init__(self, email, passwd):
+        self.email  = email
+        self.passwd = passwd
+        self.br     = Browser('chrome')
+        # to dynamically load jQuery into the HTML head
+        self.loadjq = """var head = document.getElementsByTagName('head')[0];
+                   var script  = document.createElement('script');
+                   script.type = 'text/javascript';
+                   script.src  = '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js';
+                   head.appendChild(script);"""
+
+        self.login()
+
+
+    def create(self, title, desc, date, time):
+        """ Create a new Google Plus event """
+        self.br.find_by_css('div[guidedhelpid="events_create_event_button"]')[0].click()
+
+        self.br.find_by_css('input[placeholder="Event title"]').fill(title)
+
+        self.br.find_by_css('input[class="g-A-G T4 lUa"]').click()
+        self.br.execute_script('document.body.getElementsByClassName("g-A-G T4 lUa")[0].value = ""')
+        self.br.find_by_css('input[class="g-A-G T4 lUa"]').type('{}\t'.format(date))
+
+        self.br.execute_script(self.loadjq)
+        loaded = False
+        while not loaded:
+            try:
+                self.br.execute_script('$(".EKa")[0].value = ""')
+            except Exception, e:
+                pass
+            else:
+                loaded = True
+
+        self.br.find_by_css('input[class="g-A-G T4 EKa"]')[0].type('{}'.format(time))
+
+        self.br.execute_script('document.body.getElementsByClassName("yd editable")[1].innerHTML = "{}"'.format(desc))
+
+        self.br.find_by_css('div[guidedhelpid="sharebutton"]').click()
+        self.br.find_by_css('input[class="i-j-h-G-G"]').type('Public\t')
+        self.br.find_by_css('div[guidedhelpid="sharebutton"]').click()
+
+        sleep(4)  # wait for double page load
+        return self.br.url  # return event url
+
+
+    def update(self, id, title=None, desc=None, date=None, time=None):
+        """ Update a Google Plus event """
+        """ TODO: Refactor most of this into create method to reduce redundancy """
+        self.br.visit(id)
+        # click dropdown to see edit event link
+        self.br.find_by_xpath('//*[@id="contentPane"]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div/div[1]').click()
+        self.br.find_by_xpath('//*[@id=":o"]/div').click()
+
+        if title:
+            self.br.find_by_css('input[placeholder="Event title"]').fill(title)
+        if date:
+            self.br.find_by_css('input[class="g-A-G T4 lUa"]').click()
+            self.br.execute_script('document.body.getElementsByClassName("g-A-G T4 lUa")[0].value = ""')
+            self.br.find_by_css('input[class="g-A-G T4 lUa"]').type('{}\t'.format(date))
+        if time:
+            self.br.execute_script(self.loadjq)
+            loaded = False
+            while not loaded:
+                try:
+                    self.br.execute_script('$(".EKa")[0].value = ""')
+                except Exception, e:
+                    pass
+                else:
+                    loaded = True
+
+            self.br.find_by_css('input[class="g-A-G T4 EKa"]')[0].type('{}'.format(time))     
+        if desc:
+            self.br.execute_script('document.body.getElementsByClassName("yd editable")[1].innerHTML = "{}"'.format(desc))
+
+
+        self.br.find_by_css('div[guidedhelpid="sharebutton"]').click()
+
+
+    def details(self, id):
+        """ Read details of a Google event """
+        self.br.visit(id)
+        details = {}
+        details['title'] = self.br.find_by_css('div[class="Iba"]').text.split('\n')[0]
+        details['desc']  = self.br.find_by_css('div[class="T7BsYe"]').text
+
+        print details
+        return details
+
+
+    def login(self):
+        self.br.visit('https://plus.google.com/u/0/events')
+        self.br.fill('Email', self.email)
+        self.br.fill('Passwd', self.passwd)
+        self.br.find_by_name('signIn').click()
+
+with open('config.json', 'r') as config:
+    settings = json.loads(config.read())
+
+gpem  = GPlusEventManager(settings['username'], settings['password'])
+event = gpem.create('test', 'test', '2013-09-01', '10:45 PM')
+print event
+gpem.update(event, title='New title')
+sleep(2)
+gpem.details(event)
+
