a67e10fe7adaa4ee5b4bac6d4f14bc3c32faf246
Kendrick Ledet <kendrickledet@gmail.com>
2013-07-02 11:45:44 -0500

    removed redundant file test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-from easyprocess import EasyProcess
-from pyvirtualdisplay.smartdisplay import SmartDisplay
-with SmartDisplay(visible=0, bgcolor='black') as disp:
-    EasyProcess('python gplus_event.py create --title "test" --description description.txt --date "2012-06-09 10PM"').call()
